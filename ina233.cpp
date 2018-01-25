a09a9dd471d1a40d2dd7c81e4ed20f374b4b10eb
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2018-01-24 20:45:16 -0500

    power accumulator readout - seems to be working?

--- ina233.cpp
+++ ina233.cpp
@@ -8,46 +8,49 @@
 #include "ina233.h"
 #include "Wire.h"
 
-ina233::ina233(int address)
+ina233::ina233(int address, double Rshunt, double Imax)
 {
 	_address = address;
 	Wire.begin();
+	
+	// temp
+	_inaBusVoltageSamplePeriod = 0.0011;		// INA233 default 1.1ms
+	_inaShuntVoltageSamplePeriod = 0.0011;	// INA233 default 1.1ms
 }
 
-int readBusVoltageCode()
+int ina233::readBusVoltageCode()
 {
 	int code = _readTwoByteRegister( READ_VIN );
 	return code;
 }
 
-int readShuntVoltageCode()
+int ina233::readShuntVoltageCode()
 {
 	int code = _readTwoByteRegister( MFR_READ_VSHUNT );
 	return code;
 }
 
-int readPowerCode()
+int ina233::readPowerCode()
 {
 	int code = _readTwoByteRegister( READ_PIN );
 	return code;
 }
 
-int readCurrentCode()
+int ina233::readCurrentCode()
 {
 	int code = _readTwoByteRegister( READ_IIN );
 	return code;
 }
 
-double readPower()
+double ina233::readPower()
 {
 	int code = readPowerCode();
-	// TODO set scaling appropriately by current config
-	double power = 0.0;
+	double power = code * _powerLSB;
 	
 	return power;
 }
 
-double readBusVoltage()
+double ina233::readBusVoltage()
 {
 	int code = readBusVoltageCode();
 	double busVoltage = code * VIN_LSB_VALUE_V;
@@ -55,7 +58,7 @@ double readBusVoltage()
 	return busVoltage;
 }
 
-double readShuntVoltage()
+double ina233::readShuntVoltage()
 {
 	int code = readShuntVoltageCode();
 	double shuntVoltage = code * VSHUNT_LSB_VALUE_V;
@@ -63,20 +66,19 @@ double readShuntVoltage()
 	return shuntVoltage;
 }
 
-double readCurrent()
+double ina233::readCurrent()
 {
 	int code = readCurrentCode();
-	// TODO set scaling appropriately by current config
-	double current = 0.0;
+	double current = code * _currentLSB;
 	
 	return current;
 }
 
-void configureADC(char avgMode, char busConvTime, char shuntConvTime )
+void ina233::configureADC(char avgMode, char busConvTime, char shuntConvTime )
 {
-	// Read current ADC settings
-	int settings = 0; 	// todo
-	settings = settings & 0b0000000000000111 	// want to keep power mode the same
+	// TODO Read current ADC settings
+	int settings = 0;		// todo
+	settings = settings & 0b0000000000000111;		// want to keep power mode the same
 	
 	settings |= (shuntConvTime & 0b111) << 3;
 	settings |= (busConvTime & 0b111) << 6;
@@ -85,49 +87,140 @@ void configureADC(char avgMode, char busConvTime, char shuntConvTime )
 	// TODO write settings back to device
 }
 
-void clearFaults()
+uint16_t ina233::readADCConfiguration()
+{
+	uint16_t reg = _readTwoByteRegister( MFR_ADC_CONFIG );
+	/*
+	//debug
+	Serial.print("ADC conf: ");
+	Serial.print(reg, BIN);
+	Serial.print("\n");
+	*/
+
+	return reg;
+}
+
+/*
+	Update the instance's energy counter (lastAccumulatorReadTime, etc). Clear the INA233 accumulator.
+	
+	This function uses Arduino's micros() for time tracking since the INA233 datasheet warns that the internal oscillator variance can result in "energy error up to 10%". Scary.
+	
+	No return values, read what you care about using the <TBD> methods. 
+ */
+void ina233::readAccumulator()
+{
+	uint32_t	powerAccumulator;
+	uint32_t	sampleCount;
+	uint32_t	accumulatorReadTime;
+	double	averagePower;
+	double	averageEnergy;
+	
+	Wire.beginTransmission( _address );
+	Wire.write( READ_EIN );
+	Wire.endTransmission( false );		// repeated START
+	Wire.requestFrom( _address, 7, true );
+	
+	accumulatorReadTime = micros();		// save the timestamp here - not quite
+										//	sure when the INA233 commits the data
+										//	but this is a good compromise for now
+						
+	// First three bytes: power accumulator value and overflow count
+	while( !Wire.available() ){ yield(); }; 	// wait for reply
+	powerAccumulator = Wire.read();				// junk byte - trash it?
+	Serial.print("this should be 6: ");
+	Serial.print(powerAccumulator, DEC);
+	Serial.print("\n");	 
+					 
+	while( !Wire.available() ){ yield(); }; 	// wait for reply
+	powerAccumulator = Wire.read();				// accumulator LSB
+	while( !Wire.available() ){ yield(); }; 	// wait for next byte
+	powerAccumulator += Wire.read() << 8;		// accumulator MSB
+	while( !Wire.available() ){ yield(); }; 	// wait for next byte
+	powerAccumulator += Wire.read() << 16;		// accumulator rollover count
+	
+	// Next three bytes: sample counter value 
+	while( !Wire.available() ){ yield(); }; 	// wait for reply
+	sampleCount = Wire.read();					// sample count low byte
+	while( !Wire.available() ){ yield(); }; 	// wait for next byte
+	sampleCount += Wire.read() << 8;			// sample count mid byte
+	while( !Wire.available() ){ yield(); }; 	// wait for next byte
+	sampleCount += Wire.read() << 16;			// sample count high byte 
+	
+	clearAccumulator();
+	
+	// Convert values to real-world units
+	averagePower = (double)powerAccumulator;	//cast to double first to avoid mult overflow
+	averagePower = averagePower * _powerLSB;	// UMMM does this make sense?
+	
+	// debug: compare INA233 clock to Arduino clock
+	double intervalArduino = (double)(accumulatorReadTime - lastAccumulatorReadTime) * 0.000001;
+	double intervalINA = (double)(sampleCount) * (_inaBusVoltageSamplePeriod + _inaShuntVoltageSamplePeriod);
+	Serial.print("Arduino interval: ");
+	Serial.print(intervalArduino, DEC);
+	Serial.print(" INA interval: ");
+	Serial.print(sampleCount, DEC);
+	Serial.print("/");
+	Serial.print(intervalINA, DEC);
+	Serial.print("\nError: ");
+	Serial.print(intervalArduino - intervalINA, DEC);
+	Serial.print(" sec\n");
+	// end debug
+
+	lastAccumulatorReadTime = accumulatorReadTime;
+	
+}
+
+void ina233::clearAccumulator()
+{
+	Wire.beginTransmission( _address );
+	Wire.write( CLEAR_EIN );
+	Wire.endTransmission();
+	return;
+}
+
+void ina233::clearFaults()
 {
 	// TODO
+	return;
 }
 
 /* 
 	Automagically configure the MFR_CALIBRATION register that sets the scaling factor inside the INA233.
 	Arguments:
-		Rshunt: value of the shunt resistor connected to the device, in ohms
-		Imax: maximum expected current, in amps (single-sided, so +-150A measurement -> Imax = 150)
+	Rshunt: value of the shunt resistor connected to the device, in ohms
+	Imax: maximum expected current, in amps (single-sided, so +-150A measurement -> Imax = 150)
 	This function does the math internally and sets the configuration register according to TI's datasheet recommendations. If you want full control over the exact calibration value to be set, you should do the math offline and set the register with your own static value.
  */
-void configureShuntValue( double Rshunt, double Imax )
+void ina233::configureShuntValue( double Rshunt, double Imax )
 {
 	_currentLSB = Imax/32768;
-	_powerLSB = _currentLSB*25;							// fixed inside INA233
-	int cal = (int)(0.00512/(_currentLSB*Rshunt));		// per TI datasheet
+	_powerLSB = _currentLSB*25;				// fixed inside INA233
+	int cal = (int)(0.00512/(_currentLSB*Rshunt));	// per TI datasheet
 	
 	// Write value to cal register
 	Wire.beginTransmission( _address );
 	Wire.write( MFR_CALIBRATION );
-	Wire.write( (uint8_t)(cal & 0x00FF) );				// write LSB
-	Wire.write( (uint8_t)((cal & 0xFF00)>>8) );			// write MSB
+	Wire.write( (uint8_t)(cal & 0x00FF) );		// write LSB
+	Wire.write( (uint8_t)((cal & 0xFF00)>>8) );		// write MSB
 	Wire.endTransmission();
 	
 	//TODO read the value back lol
 	
 }
 
-int _readTwoByteRegister( int register )
+int ina233::_readTwoByteRegister( int reg )
 {
 	int code = 0;
 	
-	Wire.beginTransmission( _address );			// send dev address
-	Wire.write( register );						// send register
-	Wire.endTransmission( false );				// repeated START
-	Wire.requestFrom( _address, 2, true );		// read data
+	Wire.beginTransmission( _address );		// send dev address
+	Wire.write( reg );			// send register
+	Wire.endTransmission( false );		// repeated START
+	Wire.requestFrom( _address, 2, true );	// read data
 	
-	while( !Wire.available() ){ yield(); };		// wait for reply
-	code += Wire.read();						// LSB is sent first
-	while( !Wire.available() ){ yield(); };		// wait for reply
-	code += Wire.read() << 8;					// then MSB
-												// TODO: what if it doesn't reply?
+	while( !Wire.available() ){ yield(); }; // wait for reply
+	code += Wire.read();				// LSB is sent first
+	while( !Wire.available() ){ yield(); }; // wait for reply
+	code += Wire.read() << 8;			// then MSB
+						// TODO: what if it doesn't reply?
 	return code;
 }
-
--- ina233.h
+++ ina233.h
@@ -13,56 +13,70 @@ class ina233
 {
 	public:
 		ina233(int address, double Rshunt, double Imax);		//TODO init options
-		int readBusVoltageCode();
-		int readShuntVoltageCode();
-		int readPowerCode();
-		int readCurrentCode();
+	 
+		int			readBusVoltageCode();
+		int			readShuntVoltageCode();
+		int			readPowerCode();
+		int			readCurrentCode();
+		uint16_t 	readADCConfiguration();
 		
-		double readBusVoltage();
-		double readShuntVoltage();
-		double readPower();
-		double readCurrent();
+		double		readBusVoltage();
+		double		readShuntVoltage();
+		double		readPower();
+		double		readCurrent();
+
+		uint32_t	lastAccumulatorReadTime;		// micros() output from last accum read
+		int32_t		lastAccumulatorRead;			// last value read from power accumulator
+		uint32_t	lastAccumulatorSamples;			// number of samples reported in last accumulator read
+			
+		double		lastAveragePower;
+		double		lastEnergy;
 		
-		void configureADC(char avgMode, char busConvTime, char shuntConvTime );
-		void configureShuntValue( double Rshunt, double Imax );
-		void clearFaults();
+		void		configureADC(char avgMode, char busConvTime, char shuntConvTime );
+		void		configureShuntValue( double Rshunt, double Imax );
+		void		clearFaults();
+		void		clearAccumulator();
+		void		readAccumulator();
+		
 	private:
-		int _readTwoByteRegister();
-		int _address;
-		double _Rshunt;					// value of shunt resistor
-		double _Imax;					// full-scale current chosen
-		double _currentLSB;
-		double _powerLSB;
+		int			_readTwoByteRegister( int register );
+		int			_address;
+		double		_Rshunt;						// value of shunt resistor
+		double		_Imax;							// full-scale current chosen
+		double		_currentLSB;
+		double		_powerLSB;
+		float		_inaBusVoltageSamplePeriod;
+		float		_inaShuntVoltageSamplePeriod;
 		// TODO configuration settings
 };
 
 // Register addresses
-int const CLEAR_FAULTS 			= 0x03;
-int const RESTORE_DEFAULT_ALL 	= 0x12;
-int const CAPABILITY 			= 0x19;
-int const IOUT_OC_WARN_LIMIT 	= 0x4A;
-int const VIN_OV_WARN_LIMIT 	= 0x57;
-int const VIN_UV_WARN_LIMIT 	= 0x58;
-int const PIN_OP_WARN_LIMIT 	= 0x6B;
-int const STATUS_BYTE 			= 0x78;
-int const STATUS_WORD 			= 0x79;
-int const STATUS_IOUT 			= 0x7B;
-int const STATUS_INPUT 			= 0x7C;
-int const STATUS_CML 			= 0x7E;
-int const STATUS_MFR_SPECIFIC 	= 0x80;
-int const READ_EIN 				= 0x86;
-int const READ_VIN 				= 0x88;
-int const READ_IN 				= 0x89;
-int const READ_PIN 				= 0x97;
-int const MFR_ADC_CONFIG 		= 0xD0;
-int const MFR_READ_VSHUNT  		= 0xD1;
-int const MFR_ALERT_MASK 		= 0xD2;
-int const MFR_CALIBRATION 		= 0xD4;
-int const MFR_DEVICE_CONFIG  	= 0xD5;
-int const CLEAR_EIN 			= 0xD6;
+int const CLEAR_FAULTS			= 0x03;
+int const RESTORE_DEFAULT_ALL	= 0x12;
+int const CAPABILITY			= 0x19;
+int const IOUT_OC_WARN_LIMIT	= 0x4A;
+int const VIN_OV_WARN_LIMIT		= 0x57;
+int const VIN_UV_WARN_LIMIT		= 0x58;
+int const PIN_OP_WARN_LIMIT		= 0x6B;
+int const STATUS_BYTE			= 0x78;
+int const STATUS_WORD			= 0x79;
+int const STATUS_IOUT			= 0x7B;
+int const STATUS_INPUT			= 0x7C;
+int const STATUS_CML			= 0x7E;
+int const STATUS_MFR_SPECIFIC	= 0x80;
+int const READ_EIN				= 0x86;
+int const READ_VIN				= 0x88;
+int const READ_IIN				= 0x89;
+int const READ_PIN				= 0x97;
+int const MFR_ADC_CONFIG		= 0xD0;
+int const MFR_READ_VSHUNT		= 0xD1;
+int const MFR_ALERT_MASK		= 0xD2;
+int const MFR_CALIBRATION		= 0xD4;
+int const MFR_DEVICE_CONFIG		= 0xD5;
+int const CLEAR_EIN				= 0xD6;
 
 // Scaling factors
-double const VIN_LSB_VALUE_V 	= 0.00125;
+double const VIN_LSB_VALUE_V	= 0.00125;
 double const VSHUNT_LSB_VALUE_V = 0.0000025;
 
 #endif
